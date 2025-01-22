b3ef960fc0e7584fd1e7e6319d7cb84cec7a1d57
PRASHAM <prashamshyabanepali20@gmail.com>
2025-01-22 21:06:18 +0545

    Q3push

---
+++ Assignment2/Q3/Q3.js
@@ -0,0 +1,4 @@
+import { toUpperCase, toLowerCase } from "./utility.mjs";
+const s="Prashamshya Banepali";
+console.log(`lowercase:${toLowerCase(s)}`)
+console.log(`Uppercase: ${toUpperCase(s)}`)
---
+++ Assignment2/Q3/utility.mjs
@@ -0,0 +1,8 @@
+export function toUpperCase(string)
+{
+return string.toUpperCase();
+}
+export function toLowerCase(string)
+{
+    return string.toLowerCase();
+}
